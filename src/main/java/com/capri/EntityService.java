b3b1ab9127e6c81bf8db13ef040e78ecc56efbca
I. Kapri <my@mail.com>
2023-10-22 18:46:13 +0200

    refactor code

--- src/main/java/com/capri/EntityService.java
+++ src/main/java/com/capri/EntityService.java
@@ -8,41 +8,49 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
 import javax.persistence.RollbackException;
 import javax.transaction.Transactional;
 
-import com.capri.core.utils.Strings;
-
+/**
+ * Defines a generic manager for the basis CRUD operations on
+ * all the entities.
+ */
 @Transactional
-public final class EntityService<TEntity extends Serializable> {
+public class EntityService<TEntity extends Serializable> {
+  /**
+   * Contains the type of the entity.
+   */
   private final Class<TEntity> entity;
-  private static String PERSISTENCE_UNIT_NAME = null;
-  private static EntityManagerFactory ENTITY_MANAGER_FACTORY = null;
-  private static boolean IS_ENTITY_MANAGER_FACTORY_CREATED = false;
 
+  /**
+   * Contains the entity manager factory for the given persistence unit.
+   */
+  private static final EntityManagerFactory managerFactory = Persistence
+      .createEntityManagerFactory("pubdta");
+
+  /**
+   * Initializes a new instance of the {@code EntityService} class.
+   *
+   * @param entity Contains the entity to manage.
+   */
   public EntityService(Class<TEntity> entity) {
     this.entity = entity;
   }
 
-  public static void setPersistenceUnit(String unitName) throws Exception {
-    if (IS_ENTITY_MANAGER_FACTORY_CREATED) {
-      throw new Exception("Persistence unit \"" + unitName + "\" has already been set.");
-    }
-
-    if (!Strings.isNullOrWhiteSpace(unitName)) {
-      IS_ENTITY_MANAGER_FACTORY_CREATED = true;
-      PERSISTENCE_UNIT_NAME = unitName;
-      ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-    }
-  }
-
-  public <TKey extends Object> boolean delete(TKey key) {
-    TEntity entity = get(key);
-    EntityManager manager = null;
+  /**
+   * Deletes the entity with the given id.
+   *
+   * @param id Contains the id of the entity to be deleted.
+   * @return whether the entity with the given id was deleted.
+   */
+  public <TKey extends Object> boolean delete(TKey id) {
+    TEntity entity = getById(id);
     EntityTransaction transaction = null;
+    EntityManager manager = null;
 
     try {
-      manager = getEntityManager();
+      manager = managerFactory.createEntityManager();
       transaction = manager.getTransaction();
       transaction.begin();
       if (manager.contains(entity)) {
@@ -52,26 +60,26 @@ public final class EntityService<TEntity extends Serializable> {
       }
       transaction.commit();
       return true;
-    } catch (IllegalStateException exception) {
-      transaction.rollback();
-      return false;
-    } catch (RollbackException exception) {
-      transaction.rollback();
-      return false;
-    } catch (Exception exception) {
-      transaction.rollback();
+    } catch (IllegalStateException | RollbackException exception) {
+      rollbackTransaction(transaction);
       return false;
     } finally {
-      manager.close();
+      closeManager(manager);
     }
   }
 
+  /**
+   * Persists the given entity to the database and returns a `true` value
+   * in case the transaction is committed successfully and `false` otherwise.
+   *
+   * @param entity Contains the entity to be persisted in the database.
+   * @return whether the given entity has been persisted.
+   */
   public TEntity create(TEntity entity) {
-    EntityManager manager = null;
     EntityTransaction transaction = null;
-
+    EntityManager manager = null;
     try {
-      manager = getEntityManager();
+      manager = managerFactory.createEntityManager();
       transaction = manager.getTransaction();
       transaction.begin();
       if (manager.contains(entity))
@@ -79,104 +87,112 @@ public final class EntityService<TEntity extends Serializable> {
       manager.persist(entity);
       transaction.commit();
       return entity;
-    } catch (IllegalStateException exception) {
-      transaction.rollback();
-      return null;
-    } catch (RollbackException exception) {
-      transaction.rollback();
-      return null;
-    } catch (Exception exception) {
-      transaction.rollback();
+    } catch (IllegalStateException | RollbackException exception) {
+      rollbackTransaction(transaction);
       return null;
     } finally {
-      manager.close();
+      closeManager(manager);
     }
   }
 
+  /**
+   * Gets the entire list of entities of the given type from the database.
+   * 
+   * @return all the entities of the given type.
+   */
   public List<TEntity> getAll() {
-    EntityManager manager = null;
     EntityTransaction transaction = null;
     List<TEntity> resultList = null;
     List<TEntity> emptyList = new ArrayList<TEntity>();
     final String query = "SELECT e FROM " + this.entity.getSimpleName() + " e";
-
+    EntityManager manager = null;
     try {
-      manager = getEntityManager();
+      manager = managerFactory.createEntityManager();
       transaction = manager.getTransaction();
       transaction.begin();
       resultList = manager.createQuery(query, this.entity).getResultList();
       transaction.commit();
       return resultList != null ? resultList : emptyList;
-    } catch (IllegalStateException exception) {
-      transaction.rollback();
-      return emptyList;
-    } catch (RollbackException exception) {
-      transaction.rollback();
-      return emptyList;
-    } catch (Exception exception) {
-      transaction.rollback();
+    } catch (IllegalStateException | RollbackException exception) {
+      rollbackTransaction(transaction);
       return emptyList;
     } finally {
-      manager.close();
+      closeManager(manager);
     }
   }
 
-  public <TKey extends Object> TEntity get(TKey key) {
-    EntityManager manager = null;
+  /**
+   * Gets a given entity by id.
+   *
+   * @param id Contains the id of the entity to be gotten.
+   * @return the entity with the given id.
+   */
+  public <TKey extends Object> TEntity getById(TKey id) {
     EntityTransaction transaction = null;
     TEntity result = null;
-
+    EntityManager manager = null;
     try {
-      manager = getEntityManager();
+      manager = managerFactory.createEntityManager();
       transaction = manager.getTransaction();
       transaction.begin();
-      result = manager.find(this.entity, key);
+      result = manager.find(this.entity, id);
       transaction.commit();
       return result;
-    } catch (IllegalStateException exception) {
-      transaction.rollback();
-      return null;
-    } catch (RollbackException exception) {
-      transaction.rollback();
-      return null;
-    } catch (Exception exception) {
-      transaction.rollback();
+    } catch (IllegalStateException | RollbackException exception) {
+      rollbackTransaction(transaction);
       return null;
     } finally {
-      manager.close();
+      closeManager(manager);
     }
   }
 
+  /**
+   * Updates the given entity in the database.
+   * 
+   * @param entity Contains the entity to be updated.
+   * @return whether the entity was edited successfully in the database.
+   */
   public TEntity update(TEntity entity) {
-    EntityManager manager = null;
     EntityTransaction transaction = null;
-
+    EntityManager manager = null;
     try {
-      manager = getEntityManager();
+      manager = managerFactory.createEntityManager();
       transaction = manager.getTransaction();
       transaction.begin();
       manager.merge(entity);
       transaction.commit();
       return entity;
-    } catch (IllegalStateException exception) {
-      transaction.rollback();
-      return null;
-    } catch (RollbackException exception) {
-      transaction.rollback();
-      return null;
-    } catch (Exception exception) {
-      transaction.rollback();
+    } catch (IllegalStateException | RollbackException exception) {
+      rollbackTransaction(transaction);
       return null;
     } finally {
-      manager.close();
+      closeManager(manager);
     }
   }
 
-  private static EntityManager getEntityManager() throws IllegalStateException, NullPointerException {
-    if (ENTITY_MANAGER_FACTORY == null) {
-      throw new NullPointerException("Persistence unit name is not defined.");
+  /**
+   * Closes the entity manager.
+   */
+  private static final void closeManager(EntityManager manager) {
+    if (manager.isOpen()) {
+      try {
+        manager.close();
+      } catch (IllegalStateException e) {
+        // do nothing
+      }
     }
+  }
 
-    return ENTITY_MANAGER_FACTORY.createEntityManager();
+  /**
+   * Rolls back the transactiojn in case it is active.
+   */
+  private static final void rollbackTransaction(EntityTransaction transaction) {
+    if (transaction.isActive()) {
+      try {
+        transaction.rollback();
+      } catch (PersistenceException | IllegalStateException e) {
+        // do nothing
+      }
+    }
   }
 }
